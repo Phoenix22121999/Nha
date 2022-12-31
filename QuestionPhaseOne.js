2095438307603b12a6960d7198256d0761809f8f
DatNguyen <datnguyen@kie.io>
2022-12-31 13:23:34 +0700

    update question phase one

--- QuestionPhaseOne.js
+++ QuestionPhaseOne.js
@@ -1,4 +1,4 @@
-const goodAnwser = "Rất tốt. Bạn cứ tiếp tục phát huy nhé";
+const goodAnwser = "Tốt. Cứ tiếp tục phát huy nhé";
 const pleaseStop =
 	"Please stop!!! Làm như vậy sẽ chỉ tổ khiến bạn mất thời gian và hại sức khỏe của bạn thôi. Mỗi khi bị ovtk, mình hay hít thở sâu, uống một ly nước ấm, thiền, chơi thể thao, tụ tập với bạn bè để bình tâm lại tâm trí của chính mình";
 const QuestionPhaseOne = [
@@ -10,28 +10,12 @@ const QuestionPhaseOne = [
 				reply: goodAnwser,
 			},
 			{
-				content: "Trầm cảm ",
-				reply: "Bạn nên chú ý sức khỏe của bản thân hơn và bạn nên đến gặp bác sĩ, chuyên gia để có lời khuyên tốt nhất",
+				content: "Không ổn",
+				reply: "Bạn hãy chú ý sức khỏe thể chất lẫn tinh thần của bản thân. Nếu có vấn đề gì nghiêm trọng thì nên đến gặp bác sĩ nhé",
 			},
 		],
 	},
-	{
-		question: "Dạo này, bạn có ăn uống đầy đủ không?",
-		answers: [
-			{
-				content: "Ăn uống ngày đủ 3 bữa ",
-				reply: goodAnwser,
-			},
-			{
-				content: "Thích thì ăn không thì thôi ",
-				reply: "Bạn nên chú ý ăn đầy đủ bữa hơn để cơ thể hoạt động bình thường",
-			},
-			{
-				content: "Chả muốn ăn uống gì cả ",
-				reply: "Bạn nên chú ý đến sức khỏe của bạn thân hơn. Bạn phải ăn uống đầy đủ để có sức khỏe và cơ thể khỏe mạnh",
-			},
-		],
-	},
+
 	{
 		question: "Gần đây bạn thường ngủ vào mấy giờ?",
 		answers: [
@@ -41,36 +25,86 @@ const QuestionPhaseOne = [
 			},
 			{
 				content: "Sau 12 giờ đêm ",
-				reply: "Bạn nên chú ý sức khỏe và ngủ sớm hơn để có cơ thể khỏe mạnh",
+				reply: "Ngủ muộn rất có hại cho sức khỏe, khiến cơ thể thiếu năng lượng, mệt mỏi và giảm sức đề kháng. Vì vậy, hãy sắp xếp thời gian ngủ sớm hơn nhé",
 			},
 			{
 				content: "Bạn ngủ vào buổi sáng và thức đêm làm việc ",
-				reply: "Nếu bạn là một Night Owl, làm việc hiệu quả vào ban đêm thì bạn nên sắp xếp thời gian nghỉ ngơi và làm việc hợp lý nha",
+				reply: "Nếu bạn là cú đêm thì nhớ dành thời gian để nghỉ vào buổi sáng nhé",
 			},
 			{
 				content: "Bị mất ngủ nhiều ngày ",
-				reply: "Bạn nên chú ý sức khỏe của bản thân mình và ngủ đủ giấc hơn, mình khuyên bạn nên đến gặp bác sĩ để có lời khuyên tốt nhất",
+				reply: "Hãy tìm nguyên nhân và giải quyết nó. Bạn nên chú ý sức khỏe của bản thân mình. Nếu vẫn không khả quan, bạn nên đến gặp chuyên gia để có lời khuyên tốt nhất",
 			},
 		],
 	},
 	{
-		question: "Khi bị Overthinking bạn có hay sử dụng social media không?",
+		question: "Khi bị Overthinking bạn có hay sử dụng mạng xã hội không",
 		answers: [
 			{
-				content: "Có. Luôn luôn.",
-				reply: pleaseStop,
-			},
-			{
-				content: "Có. Thỉnh thoảng ",
-				reply: pleaseStop,
+				content: "Có.",
+				reply: "Dừng lại ngay! Nó chỉ khiến bạn mệt mỏi và nặng nề hơn mà thôi. Thay vào đó, bạn hãy hít thở sâu, uống một ly nước, thiền định tâm trí của mình",
 			},
 			{
 				content: "Không",
-				reply: "Rất tốt. Ít ai làm được như bạn. Mỗi khi bị ovtk, mình hay hít thở sâu, uống một ly nước ấm, thiền, chơi thể thao, tụ tập với bạn bè để bình tâm lại tâm trí của chính mình",
+				reply: "Rất tốt. Mỗi khi bị overthiking, bạn hãy hít thở sâu, uống một ly nước, thiền, chơi thể thao, tụ tập với bạn bè để bình tâm lại tâm trí của chính mình",
+			},
+		],
+	},
+	{
+		question: "Dạo này, bạn có ăn uống đầy đủ không?",
+		answers: [
+			{
+				content: "Ăn uống ngày đủ 3 bữa ",
+				reply: "Rất tốt. Tiếp tục phát huy nhé!",
 			},
 			{
-				content: "Bị mất ngủ nhiều ngày ",
-				reply: "Bạn nên chú ý sức khỏe của bản thân mình và ngủ đủ giấc hơn, mình khuyên bạn nên đến gặp bác sĩ để có lời khuyên tốt nhất",
+				content: "Bữa ăn, bữa nhịn",
+				reply: "Bạn nên chú ý ăn đầy đủ để có cơ thể khỏe mạnh",
+			},
+			{
+				content: "Chả muốn ăn uống gì cả ",
+				reply: "Bạn nên chú ý đến sức khỏe của bạn thân. Bạn phải ăn uống đầy đủ để có sức khỏe và cơ thể khỏe mạnh",
+			},
+		],
+	},
+	{
+		question: "Bạn có cảm thấy như cả thế giới đang chống lại mình không?",
+		answers: [
+			{
+				content: "Có",
+				reply: "Bạn hãy xem nó là một phần tất yếu cuộc sống. Mỗi khi buồn bạn có thể hít thở sâu, tâm sự với bạn bè hoặc đi chơi chẳng hạn",
+			},
+			{
+				content: "Không",
+				reply: "Tốt. Tiếp tục phát huy nhé!",
+			},
+		],
+	},
+	{
+		question:
+			"Gần đây, bạn có cảm thấy mệt mỏi, kiệt sức khi làm việc không?",
+		answers: [
+			{
+				content: "Có",
+				reply: "Bạn nên chú ý sức khỏe và chia thời gian nghỉ ngơi hợp lý thì mới có sức khỏe tốt để làm việc",
+			},
+			{
+				content: "Không",
+				reply: "Tuyệt. Bạn nhớ giữ gìn sức khỏe bản thân mình nhé",
+			},
+		],
+	},
+	{
+		question:
+			"Bạn có tâm sự gì giữ trong lòng nhưng không muốn nói ra không?",
+		answers: [
+			{
+				content: "Có",
+				reply: "Bạn chia sẻ vấn đề bạn gặp phải thay vì cứ giữ kín trong lòng. Vì việc nói ra những điều chất chứa trong lòng giúp ta giải tỏa cảm xúc, tháo gỡ mọi khúc mắc và suy nghĩ lạc quan hơn",
+			},
+			{
+				content: "Không có",
+				reply: "Tốt. Tiếp tục phát huy nhé",
 			},
 		],
 	},
